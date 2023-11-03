5b591978de5058a0baffa90486991a312d7ec635
hennessy-on-melody <attempt3699@gmail.com>
2023-11-03 15:37:45 +0900

    initial commit

---
+++ src/main/java/com/banktree/banktree/BankTreeApplication.java
@@ -0,0 +1,13 @@
+package com.banktree.banktree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banktree/banktree/BankTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banktree.banktree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
